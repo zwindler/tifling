9915917607b5aef2fac4599653a5e106d03bf631
Denis Germain <dt.germain@gmail.com>
2023-10-26 17:29:21 +0200

    chore: add Version variable

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/zwindler/tifling/pkg/embedTifhair"
 )
 
+var Version string
+
 func main() {
 	data, err := embedTifhair.GetDataFromJSON("json/coiffeurs.json")
 	if err != nil {
@@ -19,6 +21,7 @@ func main() {
 	randomEntry := data.Data.Features[randomIndex]
 
 	// Print the random entry
+	fmt.Printf("## tifling version %s\n\n", Version)
 	fmt.Printf("Random Entry:\n")
 	fmt.Printf("- Name: %s\n", randomEntry.Properties.Nom)
 	fmt.Printf("- Latitude/Longitude: %f %f\n", randomEntry.Properties.Lat, randomEntry.Properties.Lng)
